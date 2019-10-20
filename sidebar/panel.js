417928d7fd5cea38778e1c747935623c075b4b77
Maxine Krebs <krebs.maxine@gmail.com>
2019-10-19 22:13:24 -0500

    use fetch api to _eventually_ get list of posts

--- sidebar/panel.js
+++ sidebar/panel.js
@@ -1,57 +1,21 @@
-var myWindowId;
-const contentBox = document.querySelector("#content");
+var accessToken
 
-/*
-Make the content box editable as soon as the user mouses over the sidebar.
-*/
-window.addEventListener("mouseover", () => {
-  contentBox.setAttribute("contenteditable", true);
-});
-
-/*
-When the user mouses out, save the current contents of the box.
-*/
-window.addEventListener("mouseout", () => {
-  contentBox.setAttribute("contenteditable", false);
-  browser.tabs.query({ windowId: myWindowId, active: true }).then((tabs) => {
-    let contentToStore = {};
-    contentToStore[tabs[0].url] = contentBox.textContent;
-    browser.storage.local.set(contentToStore);
-  });
-});
-
-/*
-Update the sidebar's content.
-
-1) Get the active tab in this sidebar's window.
-2) Get its stored content.
-3) Put it in the content box.
-*/
 function updateContent() {
-  browser.tabs.query({ windowId: myWindowId, active: true })
-    .then((tabs) => {
-      return browser.storage.local.get(tabs[0].url);
+  accessToken = 'softpunk:D3B8053CA631C5166030'
+  const url = 'https://api.pinboard.in/v1/posts/all?auth_token=' + accessToken + '&format=json';
+  fetch(url, {
+    mode: 'no-cors'
+  }).then(response => response.ok)
+    .then(data => {
+      console.log(data)
     })
-    .then((storedInfo) => {
-      contentBox.textContent = storedInfo[Object.keys(storedInfo)[0]];
-    });
+    .catch(error => console.error(error))
 }
 
-/*
-Update content when a new tab becomes active.
-*/
 browser.tabs.onActivated.addListener(updateContent);
 
-/*
-Update content when a new page is loaded into a tab.
-*/
 browser.tabs.onUpdated.addListener(updateContent);
 
-/*
-When the sidebar loads, get the ID of its window,
-and update its content.
-*/
 browser.windows.getCurrent({ populate: true }).then((windowInfo) => {
-  myWindowId = windowInfo.id;
   updateContent();
 });
